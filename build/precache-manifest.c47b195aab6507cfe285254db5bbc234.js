e64c0208c9a76214d080cdfd598bfb1ca83c5f89
Dinesh Borana <dinesh.borana@freecharge.com>
2019-05-21 00:15:30 +0530

    gh pages created

---
+++ build/precache-manifest.c47b195aab6507cfe285254db5bbc234.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2014373199ca1ff95604345d1adfd7d0",
+    "url": "/stocks/index.html"
+  },
+  {
+    "revision": "0770e5197d60cbf5f1dd",
+    "url": "/stocks/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/stocks/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "0770e5197d60cbf5f1dd",
+    "url": "/stocks/static/js/main.a3bcbb89.chunk.js"
+  },
+  {
+    "revision": "bf38fc93852ff617158a",
+    "url": "/stocks/static/js/runtime~main.9551dd32.js"
+  }
+]);

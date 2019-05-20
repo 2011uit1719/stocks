280f1de61094ba50584643ccaa350cec49063806
Dinesh Borana <dinesh.borana@freecharge.com>
2019-05-20 23:14:20 +0530

    build folder added

---
+++ build/precache-manifest.b158e744d63b759f05787eca5f6ad937.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "423250825dacf7a42397233de0da9553",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94c5eb9548c06bd8b506",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "94c5eb9548c06bd8b506",
+    "url": "/static/js/main.77b5f83b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
